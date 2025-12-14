96780947dccd7561d18be11e3087213b00c8d234
dextermayhewjd <78476926+dextermayhewjd@users.noreply.github.com>
2025-12-14 21:34:51 +0800

    学习 unicode chr() ord()
    ord() 把字符转化为unicode code point
    chr（） 把 unicode code point 转化为 原本的字符表示

---
+++ cs336_basics/debug/unicode_standard.py
@@ -0,0 +1,14 @@
+x = chr(0)
+
+y = ord('牛') # 将 str 变成 code point 
+
+z = ord('s')
+print(f'print x = {x}, repr x is {repr(chr(0))}')
+# repr(chr(0))    # 返回 "'\\x00'" 
+print(f'print y = {y}, reverse y = {chr(y)}')
+
+print(f'print z = {z}, reverse z = {chr(z)}')
+
+chr(0)
+print(chr(0))
+print("this is a test" + chr(0) + "string")
