c05f598a9c138b39e872a3fa1abb65845dd23d2a
dextermayhewjd <dexterding99@icloud.com>
2025-12-27 23:09:44 +0800

    tokenizer 防御性编程 encode完成 逻辑是greedy加改循环变量 通过更改tokens 来更新循环变量条件 用字典生成式反转

--- cs336_basics/debug/2.6.1_tokenizer_encode.py
+++ cs336_basics/debug/2.6.1_tokenizer_encode.py
@@ -1,3 +1,39 @@
 from cs336_basics.debug.tokenizer import Tokenizer
+from pathlib import Path
 
-Tokenizer.from_files()
+BPE_DIR = Path("/home/dexterding/projects/assignment1-basics/bpe_outputs")
+
+OWT_MERGE_FILE = BPE_DIR /"owt_merges.pkl"
+OWT_VOCAB_FILE = BPE_DIR / "owt_vocab.pkl"
+
+# print(OWT_MERGE_FILE)
+# print(OWT_VOCAB_FILE)
+test_string = "the cat ate"
+# tokenizer = Tokenizer.from_files(vocab_filepath=OWT_VOCAB_FILE,merges_filepath=OWT_MERGE_FILE)
+
+
+vocab ={0: b' ',
+        1: b'a',
+        2: b'c',
+        3: b'e',
+        4: b'h',
+        5: b't',
+        6: b'th',
+        7: b' c',
+        8: b' a',
+        9: b'the',
+        10: b' at'}
+
+merges = [
+         (b't',b'h'),
+         (b' ',b'c'),
+         (b' ',b'a'),
+         (b'th',b'e'),
+         (b' a',b't')]
+special_tokens = ["<|speical|>"] 
+tokenizer = Tokenizer(vocab=vocab,merges=merges)
+
+# pre_tokenized_list = tokenizer._pre_tokenize(test_string)
+# print(pre_tokenized_list)
+encoding_string = tokenizer.encode(test_string)
+print(encoding_string)
--- cs336_basics/debug/tokenizer.py
+++ cs336_basics/debug/tokenizer.py
@@ -20,7 +20,7 @@ class Tokenizer:
         self.vocab = vocab
         self.merges = merges
         self.special_tokens = special_tokens or []
-        
+        self.bytes2int:dict[bytes,int] = {v:k for k,v in self.vocab.items()}
         
     @classmethod
     def from_files(
@@ -35,10 +35,79 @@ class Tokenizer:
             merges = pickle.load(f)
         
         return cls(vocab=vocab, merges=merges, special_tokens=special_tokens)
+
+    def _encode_one_token(self,token:list[bytes])->list[int]:
+        print("INPUT:", token)
+
+        tokens = list(token) # 防御式编程
+
+        
+        for a,b in self.merges:               
+            i = 0
+            new_tokens = []
+            
+            while i < len(tokens):
+                if i+1 <len(tokens) and tokens[i]==a and tokens[i+1] ==b:
+                    new_tokens.append(a+b)
+                    i+=2
+                else:
+                    new_tokens.append(tokens[i])
+                    i+=1
+                    
+            tokens = new_tokens
+        # print(tokens)    
+        return [self.bytes2int[t] for t in tokens]
+        
     
-    # def encode(self, text: str)-> list[int]:
+    def encode(self, text: str)-> list[int]:
+        ids = []
+        for bytes_seq in self._pre_tokenize(text=text):
+            ids.extend(self._encode_one_token(bytes_seq))
+        return ids
 
     # def encode_iterable(self,iterable:Iterable[str])->Iterator[int]:
     #   s 
     # def decode(self, ids:list[int])-> str:
-    #   s 
+    #   s     
+    # 
+    
+    #"You are cat<|speical|>" -> ["You are cat","<|speical|>"]
+    def _split_on_special_tokens(self,
+                                 text: str
+                                 ) -> list[str]:
+        if not self.special_tokens:
+            return [text]
+        # pattern = "|".join(re.escape(tok) for tok in special_tokens)
+        #关键：加 () 捕获组，让 special token 也出现在 split 结果里
+        pattern = "(" + "|".join(re.escape(tok) for tok in self.special_tokens) + ")"
+        
+        return [p for p in re.split(pattern, text) if p]
+    
+    # ["You are cat","<|speical|>"] 
+    # -> 
+    # [[b't', b'h', b'e'], 
+    #   [b' ', b'c', b'a', b't'], 
+    #   [b' ', b'a', b't', b'e'], 
+    #   b'<|special|>']
+    def _pre_tokenize(self,
+                      text:str, 
+                      )->list[list[bytes]]:
+            
+        pre_tokenized_list = []
+        
+        chunks = self._split_on_special_tokens(text)
+        # print(chunks)
+        special_set = set(self.special_tokens or [])
+        
+        for chunk in chunks:
+            if chunk in special_set:
+                pre_tokenized_list.append([chunk.encode("utf-8")])  
+                continue
+            
+            for m in re.finditer(self.PAT2, chunk):
+                byte_whole_token = m.group(0).encode("utf-8")
+                bytes_seq =[bytes([x]) for x in byte_whole_token]
+                print(bytes_seq)
+                pre_tokenized_list.append(bytes_seq) 
+        
+        return  pre_tokenized_list
